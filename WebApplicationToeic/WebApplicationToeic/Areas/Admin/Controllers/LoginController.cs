e33e3692abdb816041643157b1f539cdf434b475
minhandev <minhan.dev@gmail.com>
2020-03-19 21:47:03 +0700

    .

--- WebApplicationToeic/WebApplicationToeic/Areas/Admin/Controllers/LoginController.cs
+++ WebApplicationToeic/WebApplicationToeic/Areas/Admin/Controllers/LoginController.cs
@@ -3,38 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using WebApplicationToeic.Models;
+
 
 namespace WebApplicationToeic.Areas.Admin.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Admin/Login
-        toeicDbContext db = new toeicDbContext();
-        [HttpGet]
         public ActionResult Index()
         {
             return View();
-        }
-
-        [HttpPost]
-        public ActionResult Index(FormCollection form)
-        {
-            string taikhoan = form["taikhoan"].ToString();
-            string matkhau = form["matkhau"].ToString();
-            Adminstrator adminstrator = db.Adminstrators.SingleOrDefault(model => model.tKhoan == taikhoan && model.mKhau == matkhau);
-            if(adminstrator != null)
-            {
-                Session["Adminstrator"] = adminstrator.ten;
-                
-                return RedirectToAction("Index", "Home");
-            }
-            else
-            {
-                ViewBag.Error = "* Sai tài khoản hoặc mật khẩu ";
-                return View();
-            }
-            
         }
     }
 }
